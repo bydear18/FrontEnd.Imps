3e866b2ad268f4509ab0cbb5d0713cbf78500e35
bydear18 <yacademiko@gmail.com>
2024-10-22 13:46:52 +0800

    changes

--- src/pages/Headpage/HeadBody/Dashboard/HeadDashboard.jsx
+++ src/pages/Headpage/HeadBody/Dashboard/HeadDashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import './Dashboard.css';
+import './headdashboard.css';
 import { FaFileAlt, FaUser, FaCheckCircle } from 'react-icons/fa';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
